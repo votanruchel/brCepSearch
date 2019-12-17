e48d79dc4c4ef599c4e0da9b33b84f8f79aedb3d
Votan Ruchel <votanruchel@hotmail.com>
2019-12-16 21:43:09 -0300

    Name Middleware modify

--- src/middlewares/middlewareCep.js
+++ src/middlewares/middlewareCep.js
@@ -5,14 +5,16 @@ const Cep = mongoose.model('Cep');
 export default async (req,res,next) =>{
     //const c_ep = req.params.cep.replace(/\.|\-/g, '');
     const valid_cep = req.params.cep.replace('/[^0-9]/g','');
+
     if(valid_cep.length < 8 || valid_cep == '' || valid_cep === null || valid_cep === undefined){
       return res.status(400).json({ error: 'Formato invalido!' });
     }
+
     const exist = await Cep.findOne({cep: valid_cep}).select("-_id -__v -add_at");
+    
     if(exist != null){
         res.status(200).json(exist);
         return;
-    }else{
-    next();
     }
+    next();
 }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 
 import SearchController from './app/controllers/SearchController';
-import ReCheckCep from './middlewares/ReCheckCep';
+import middlewareCep from './middlewares/middlewareCep';
 
 const routes = new Router();
 
-routes.get('/search/:cep', ReCheckCep, SearchController.show);
+routes.get('/search/:cep', middlewareCep, SearchController.show);
 
 export default routes;
